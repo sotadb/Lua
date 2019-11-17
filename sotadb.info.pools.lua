94864b8218dacaa07e226fafadca80989c97c521
A Hettinger <oninoshiko@gmail.com>
2019-11-17 00:17:23 -0600

    First Commit
    This commits the developers utils and the pool output Lua scripts.

---
+++ sotadb.info.pools.lua
@@ -0,0 +1,22 @@
+
+function sotadbinfoUpdatePool()
+  sotadbinfoPoolOutput = string.format(" Adv Pool:\t%s\nProd Pool:\t%s",
+    comma_value(ShroudGetPooledAdventurerExperience()), comma_value(ShroudGetPooledProducerExperience()))
+end
+
+function comma_value(n) -- credit http://richard.warburton.it
+	local left,num,right = string.match(n,'^([^%d]*%d)(%d*)(.-)$')
+	return left..(num:reverse():gsub('(%d%d%d)','%1,'):reverse())..right
+end
+
+function ShroudOnStart()
+  ShroudRemovePeriodic("sotadbinfo.UpdatePool")
+  sotadbinfoUpdatePool()
+  ShroudRegisterPeriodic("sotadbinfo.UpdatePool", "sotadbinfoUpdatePool", 1, true)
+  sotadbinfoPoolX = ShroudGetScreenX() - 200
+  sotadbinfoPoolY = ShroudGetScreenY() - 50
+end
+
+function ShroudOnGUI()
+  ShroudGUILabel(sotadbinfoPoolX, sotadbinfoPoolY, 200, 50, sotadbinfoPoolOutput)
+end
---
+++ sotadb.info.util.lua
@@ -0,0 +1,36 @@
+sotadbinfoCommand = { }
+
+
+function sotadbinfoCommand.ShowStats()
+  for i=0, ShroudGetStatCount(), 1 do
+    ConsoleLog(string.format('%d: %s', i, ShroudGetStatNameByNumber(i)))
+  end
+  ConsoleLog(string.format("total stats: %d", ShroudGetStatCount()))
+end
+
+function sotadbinfoCommand.ShowStat(args)
+  i = tonumber(args)
+  ConsoleLog(string.format('(%d)%s: %f', i, ShroudGetStatNameByNumber(i), ShroudGetStatValueByNumber(i)))
+end
+
+function sotadbinfoCommand.findStat(args)
+  for i=0, ShroudGetStatCount(), 1 do
+    if string.match(string.lower(ShroudGetStatNameByNumber(i)), string.lower(args)) then
+      ConsoleLog(string.format('(%d) %s: %s', i, ShroudGetStatNameByNumber(i), ShroudGetStatDescriptionByNumber(i)))
+    end
+  end
+end
+
+function ShroudOnStart()
+  sotadbinfoPlayerName = ShroudGetPlayerName()
+end
+
+-- watch for commands
+function ShroudOnConsoleInput(type, src, msg)
+  if type == 'Local' and src == sotadbinfoPlayerName then
+    local cmd, arg = string.match(msg, '\\(%w+)%s+(%w*)')
+    if not pcall(sotadbinfoCommand[cmd], arg) then
+      ConsoleLog("Command Not Found")
+    end
+  end
+end
