45c37df3dbac3bce8cbbdf3f6af87fe10812f41c
A Hettinger <oninoshiko@gmail.com>
2019-11-20 21:21:05 -0600

    Add MessageWatch()
    There are so many messages, sometimes you just want to see them to
    figure out how to extract what you're looking for from the stream.

--- info.sotadb.util.lua
+++ info.sotadb.util.lua
@@ -21,22 +21,39 @@ function sotadbinfoCommand.findStat(args)
   end
 end
 
+--[[ -- ShroudGetPartyMemberNamesInScene() not working?
 function sotadbinfoCommand.PartyMembers()
   ConsoleLog("start PartyMembers()")
-  local PartyList = GetPartyMemberNamesInScene()
+  local PartyList = ShroudGetPartyMemberNamesInScene()
   ConsoleLog("mid PartyMembers()")
   for k, v in pairs(PartyList) do
     ConsoleLog(string.format("PartyMember: $d, $s", k, v))
   end
   ConsoleLog("end PartyMembers()")
 end
+ ]]
 
 function ShroudOnStart()
+  sotadbinfoMessageWatch = false
   sotadbinfoPlayerName = ShroudGetPlayerName()
+end
+
+function sotadbinfoCommand.MessageWatch(args)
+  if string.match(args, 'enable') then
+    sotadbinfoMessageWatch = true
+  elseif string.match(args, 'disable') then
+    sotadbinfoMessageWatch = false
+  else
+    ConsoleLog("Usage: \\MessageWatch [enable|disable]")
+  end
 end
 
 -- watch for commands
 function ShroudOnConsoleInput(type, src, msg)
+  if sotadbinfoMessageWatch then
+    ConsoleLog("Type: %s Src: %s Msg: %s")
+  end
+
   if type == 'Local' and src == sotadbinfoPlayerName then
     local cmd, arg = string.match(msg, '\\(%w+)%s*(%w*)')
     if not pcall(sotadbinfoCommand[cmd], arg) then
