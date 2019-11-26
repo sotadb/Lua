97f2263d3ec220f1c30907d50427bc8411d399d6
A Hettinger <oninoshiko@gmail.com>
2019-11-26 00:21:33 -0600

    Add copyright and licensing info to each file.
    If others want to contribute to this collection, it should say who wrote
    what.

--- info.sotadb.util.lua
+++ info.sotadb.util.lua
@@ -1,5 +1,28 @@
-sotadbinfoCommand = { }
+--[[
+MIT License
 
+Copyright (c) 2019 SotADB.info
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+]]
+
+sotadbinfoCommand = { }
 
 function sotadbinfoCommand.ShowStats()
   for i=0, ShroudGetStatCount(), 1 do
@@ -21,7 +44,7 @@ function sotadbinfoCommand.findStat(args)
   end
 end
 
---[[ -- ShroudGetPartyMemberNamesInScene() not working?
+-- ShroudGetPartyMemberNamesInScene() not working?
 function sotadbinfoCommand.PartyMembers()
   ConsoleLog("start PartyMembers()")
   local PartyList = ShroudGetPartyMemberNamesInScene()
@@ -31,7 +54,6 @@ function sotadbinfoCommand.PartyMembers()
   end
   ConsoleLog("end PartyMembers()")
 end
- ]]
 
 function ShroudOnStart()
   sotadbinfoMessageWatch = false
